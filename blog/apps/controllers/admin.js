b00a839911fcfb8c7d080217d11024ec490aa037
Tony Hoang <hoangtrongminh304@gmail.com>
2018-03-19 00:47:02 +0700

    add login

--- blog/apps/controllers/admin.js
+++ blog/apps/controllers/admin.js
@@ -1,6 +1,8 @@
 var express = require("express")
 var router = express.Router()
+var helper = require("../helpers/helper")
 
+var user_md = require("../models/user")
 var post_md = require("../models/post")
 
 router.get("/", function(req, res) {
@@ -19,6 +21,87 @@ router.get("/", function(req, res) {
     })
 
     // res.render("admin/dashboard", {data: {error: false}})
+})
+
+router.get("/signup", function(req, res) {
+    res.render("signup", {data: {}})
+})
+
+router.post("/signup", function(req, res) {
+    var user = req.body
+
+    if (user.email.trim().length === 0) {
+        res.render("signup", {data: {error: "Email is required"}})
+    }
+
+    if (user.passwd !== user.repasswd && user.passwd.trim().length !== 0) {
+        res.render("signup", {data: {error: "Password is not match"}})
+    }
+
+    // Insert to DB
+    var password = helper.hash_password(user.passwd)
+
+    user = {
+        email: user.email,
+        password: password,
+        first_name: user.firstname,
+        last_name: user.lastname
+    }
+
+
+    var result = user_md.addUser(user)
+
+    result.then(function(data) {
+        // res.json({message: "Insert success"})
+        res.redirect("/admin/signin")
+    })
+    .catch(function(err) {
+        res.render("signin", {data: {error: "Error"}})
+    })
+
+
+})
+
+router.get("/signin", function(req, res) {
+    res.render("signin", {data: {}})
+})
+
+router.post("/signin", function(req, res) {
+    var params = req.body
+
+    if (params.email.trim().length === 0) {
+        res.render("signin", {data: {error: "Please enter an email"}})
+    } else {
+        var data = user_md.getUserByEmail(params.email)
+
+        if (data) {
+            data.then(function(users) {
+                var user = users[0]
+
+                if (user  !== undefined) {
+
+            console.log("params.password=========", params.password)
+            console.log("user.password=========", user)
+
+                var status = helper.compare_password(params.password, user.password)
+                   if (!status) {
+                    res.render("signin", {data: {error: "Password is wrong"}}) }
+                else {
+                    req.session.user = user
+                    res.redirect("/admin/")
+                }
+            } else {
+res.render("signin", {data: {error: "User not exists"}})
+            }
+
+            })
+        } else {
+
+            res.render("signin", {data: {error: "User not exists"}})
+            }
+        }
+
+    console.log(params)
 })
 
 router.get("/post/new", function(req, res) {
--- blog/apps/models/user.js
+++ blog/apps/models/user.js
@@ -1,4 +1,41 @@
+var q = require("q")
 var db = require("../common/database")
 
 var conn = db.getConnection()
 
+function addUser(user) {
+    if (!user) {
+        return false
+    }
+
+    var defer = q.defer()
+
+    var query = conn.query('INSERT INTO users SET ?', user, function(err, result) {
+        err ? defer.reject(err) : defer.resolve(result)
+    })
+
+    return defer.promise
+}
+
+function getUserByEmail(email) {
+    if (!email) {
+        return false
+    }
+
+    var defer = q.defer()
+
+    console.log("hehe.", email)
+
+    var query = conn.query('SELECT * FROM users WHERE ?', {email: email}, function(err, result) {
+        err ? defer.reject(err) : defer.resolve(result)
+    })
+
+
+
+    return defer.promise
+}
+
+module.exports = {
+    addUser: addUser,
+    getUserByEmail: getUserByEmail
+}
